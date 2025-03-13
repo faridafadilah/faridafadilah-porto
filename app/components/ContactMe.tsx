fc842e9265b01ec57aa446ceb4cd0626f3d6a5a6
Farida Fadilah <faridafadilah@gmail.com>
2025-03-13 14:33:33 +0700

    feat: download cv

--- app/components/ContactMe.tsx
+++ app/components/ContactMe.tsx
@@ -6,6 +6,7 @@ import {
   IconBrandInstagram,
   IconBrandLinkedin,
   IconBrandWhatsapp,
+  IconDownload,
 } from "@tabler/icons-react";
 import { Label } from "~/lib/label";
 import { Input } from "~/lib/input";
@@ -167,6 +168,17 @@ export function Email() {
             </span>
             <BottomGradient />
           </a>
+          <a
+          href="/file/CV.pdf"
+          download="CV-Farida-Fadilah.pdf"
+          className="relative group/btn flex space-x-2 items-center justify-start px-4 w-full text-black rounded-md h-10 font-medium shadow-input bg-gray-50 dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_var(--neutral-800)]"
+          >
+           <IconDownload className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-neutral-700 dark:text-neutral-300 text-sm">
+              Download CV
+            </span>
+            <BottomGradient />
+        </a>
         </div>
       </div>
     );
